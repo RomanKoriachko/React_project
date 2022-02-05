6385c479e541346db208eb5007f9160413280afb
Roman <mazaxaka.tyt@gmail.com>
2022-02-05 13:35:22 +0200

    fix buttons open and close

--- src/containers/Header/MobileMenu.js
+++ src/containers/Header/MobileMenu.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { useState } from 'react'
+import { RiCloseLine } from 'react-icons/ri'
 
 export const MobileMenu = () => {
     const [open, setOpen] = useState(false)
 
+    const openBtn = (
+        <GiHamburgerMenu
+            className="mobile-menu"
+            onClick={() => setOpen(!open)}
+        />
+    )
+    const closeBtn = (
+        <RiCloseLine
+            className="mobile-menu-close"
+            onClick={() => setOpen(!open)}
+        />
+    )
+
     return (
         <>
+            {open ? closeBtn : openBtn}
             {open && (
                 <div className="menu navigation active-menu">
                     <a href="">HOME</a>
@@ -17,10 +32,6 @@ export const MobileMenu = () => {
                     <a href="">BUY NOW</a>
                 </div>
             )}
-            <GiHamburgerMenu
-                className="mobile-menu"
-                onClick={() => setOpen(!open)}
-            />
         </>
     )
 }
