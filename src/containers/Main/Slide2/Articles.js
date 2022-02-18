fca974c6007754edfb690cb6bd48db6a506f247a
Roman <mazaxaka.tyt@gmail.com>
2022-02-18 14:08:35 +0200

    finished Slide2

--- src/containers/Main/Slide2/Articles.js
+++ src/containers/Main/Slide2/Articles.js
@@ -19,6 +19,9 @@ export const Articles = () => {
                     )
                 )}
             </div>
+            <div className="more">
+                <button>DISCOVER MORE</button>
+            </div>
         </>
     )
 }
