66b26340d0fe0422acd78dadbb429956abb84843
Roman <mazaxaka.tyt@gmail.com>
2022-02-23 17:05:33 +0200

    add Footer

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -3,7 +3,12 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className="container">
-            <div className="footer"></div>
+            <div className="footer">
+                <p>
+                    © 2022 Betheme by Muffin group | All Rights Reserved |
+                    Powered by WordPress
+                </p>
+            </div>
         </div>
     )
 }
